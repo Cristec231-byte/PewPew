3400caf1f413eef1e12126ca741233602f1ce1f0
Danielle Arlantico <132239427+daniellearl@users.noreply.github.com>
2024-09-16 04:50:34 +1200

    Boss Health Bar
    Boss healthbar shows below player healthbar

--- My project/Assets/Scripts/Test/Enemy Health.cs
+++ My project/Assets/Scripts/Test/Enemy Health.cs
@@ -1,30 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EnemyHealth : MonoBehaviour
 {
     public int health;
     public int maxHealth = 10;
-    
+    public Slider slider;
+
+    [SerializeField]
+    private TextMeshProUGUI valueText;
+
     void Start()
     {
         health = maxHealth;
+        slider.maxValue = maxHealth;
+        slider.value = health;
+
+        UpdateHealthText();
+    }
+
+    private void UpdateHealthText()
+    {
+        valueText.text = $"Boss Health: {health}";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void TakeDamage(int amount)
     {
         health -= amount;
 
-        if(health <= 0)
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
+
+        health = Mathf.Clamp(health, 0, maxHealth);
+        slider.value = health;
+        UpdateHealthText();
+
+    }
+
+    internal void ChangeBossHealth(int amountToChangeStat)
+    {
+        health += amountToChangeStat;
+
+        health = Mathf.Clamp(health, 0, maxHealth);
+
+        slider.value = health;
+
+        UpdateHealthText();
     }
 }
