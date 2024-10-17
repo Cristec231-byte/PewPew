ee407697b0fc4b1cb6d8b65295a87713a5c23f22
Joy3doy <dtd8420@autuni.ac.nz>
2024-10-18 02:24:41 +1300

    fixing little bug after merging

--- My project/Assets/Scripts/AudioManager.cs
+++ My project/Assets/Scripts/AudioManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using static Unity.VisualScripting.Member;
+//using static Unity.VisualScripting.Member;
 
 public class AudioManager : MonoBehaviour
 {
